6861b915d90d4e8ecacdb30479769ad1b06c7aed
thiagocsantos <thi.coutinho.santos@gmail.com>
2022-10-21 13:15:35 -0300

    feat: Fim do Jogo

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+//esse é o script principal. funções auxiliares estão no script2
+
 let text = "Entre com um número par de cartas de 4 a 14?";
-let test = 4;
-let numeroCartas = test ? test : perguntaInicial();
+let testando =false;
+let numeroCartas = testando ? testando : perguntaInicial(text);
 let cartaSelecionadaAntes;
 const game = document.querySelector(".game");
+let numJogadas=0;
 
 let listaCartasImgFrente = [1, 2, 3, 4, 5, 6, 7];
-// embaralhamos para cada jogo ter cartas diferentes
 
+// embaralhamos para cada jogo ter cartas diferentes
 embaralhaLista(listaCartasImgFrente)
+
+// precisamos tiras algumas cartas de acordo com o que o usuário informou
 listaCartasImgFrente = listaCartasImgFrente.slice(7 - numeroCartas / 2)
+// duplicamos as lista de cartas concatenando ela com ela mesma para formar os pares
 listaCartasImgFrente = listaCartasImgFrente.concat(listaCartasImgFrente)
+//embaralha-se novamente para ter a aleatoriedade do jogo
 embaralhaLista(listaCartasImgFrente)
 
 // cada carta tem um atributo ncarta que é único do tipo n12 , onde 1 é a sua ordem no jogo, e 2 é sua imagem da frente
@@ -25,36 +32,42 @@ for (let i = 0; i < numeroCartas; i++) {
 }
 
 function fazerJogada(novaCarta){
-    console.log(novaCarta);
-    console.log(novaCarta.getAttribute("ncarta"));
+    // console.log(novaCarta);
 
     if (cartaSelecionadaAntes===novaCarta.getAttribute("ncarta")){
        // caso em que clicou na mesma carta de antes, não faço nada
-       console.log("mesma carta")
+    //    console.log("mesma carta")
     } else if(cartaSelecionadaAntes===undefined){
-        // caso em que selecionou a primeira carta
-        console.log("primeira")
+        numJogadas++
+        // caso em que selecionou a primeira carta. importante que essa condição seja testada primeiro pois 
+        // haveria erro se pegasse uma índice de variável undefined como ocorreria se os outros ifs viessem primeiro
+        // console.log("primeira")
         cartaSelecionadaAntes = novaCarta.getAttribute("ncarta")
-        console.log(cartaSelecionadaAntes)
+        // console.log(cartaSelecionadaAntes)
         virarCarta(novaCarta)
 
     } else if (cartaSelecionadaAntes[2]===novaCarta.getAttribute("ncarta")[2]){
+        numJogadas++
         // caso em que acertou o par
-        console.log(`acertou .${cartaSelecionadaAntes}`)
+        // console.log(`acertou .${cartaSelecionadaAntes}`)
+
         const carta1 = document.querySelector(`.${cartaSelecionadaAntes}`)
         virarCarta(novaCarta)
         cartaCerta(carta1)
         cartaCerta(novaCarta)
-        console.log(carta1)
-        console.log(novaCarta)
+        numeroCartas-=2
+        numeroCartas==0? setTimeout(alert,2000,`Você ganhou em ${numJogadas} jogadas`):
+        // console.log(carta1)
+        // console.log(novaCarta)
         cartaSelecionadaAntes = undefined
     } else if (cartaSelecionadaAntes[2]!==novaCarta.getAttribute("ncarta")[2]) {
+        numJogadas++
         // caso em que errou o par
-        console.log("errou")
+        // console.log("errou")
         virarCarta(novaCarta)
-        console.log(`.${cartaSelecionadaAntes}`)
+        // console.log(`.${cartaSelecionadaAntes}`)
         const carta1 = document.querySelector(`.${cartaSelecionadaAntes}`)
-        console.log(carta1)
+        // console.log(carta1)
         setTimeout(()=>{virarCarta(carta1)},1000)
         setTimeout(()=>{virarCarta(novaCarta)},1000)
         cartaSelecionadaAntes = undefined
--- script2.js
+++ script2.js
@@ -1,20 +1,26 @@
+// funções auxiliares
+
+
 function embaralhaLista(lista) {
+    // recebe uma lista e embaralha ela aleatoriamente inplace
     function comparador() { 
         return Math.random() - 0.5; 
     }
     lista.sort(comparador)
 }
 
-function perguntaInicial(){
+function perguntaInicial(texto){
+    // pergunta ao user quantas cartas e só aceita resposta número par entre 4 e 14
+    // retorna o número de cartas
     let resposta;
     while(true){
         
-        resposta = Number(prompt(text))
+        resposta = Number(prompt(texto))
         if (resposta%2==0 && resposta >=4 && resposta <=14){
             break
         }
         else {
-            text = "Resposta inválida, tente outra vez (número par entre 4 e 14):"
+            texto = "Resposta inválida, tente outra vez (número par entre 4 e 14):"
         }
     }
     return resposta
